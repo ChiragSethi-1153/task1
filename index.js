951437b3bc507f8e6247f2a00db23d0dd5f263bc
ChiragSethi50 <chiragsethi50@gmail.com>
2024-01-16 12:14:52 +0530

    modifications

--- index.js
+++ index.js
@@ -15,11 +15,20 @@ window.onclick = function (event) {
   }
 };
 
-function submitForm(e) {
+
+ let form = document.querySelector("form");
+
+ console.log("form" , form
+ )
+
+ 
+ form.addEventListener('submit', function(e){
+
+// function login(e) 
   e.preventDefault();
   let mail = document.getElementById("email").value;
-  let pass = document.getElementById("pass").value;
-
+  let password = document.getElementById("password").value;
+ 
   let data = JSON.parse(localStorage.getItem("data")) || [];
 
   if (data.length != 0) {
@@ -28,33 +37,33 @@ function submitForm(e) {
 
   const obj = {
     email: mail,
-    password: pass,
+    password: password
   };
 
   data.push(obj);
   console.log(data);
   localStorage.setItem("data", JSON.stringify(data));
-}
+});
 
-function save(e) {
-  e.preventDefault();
+// function save(e) {
+//   e.preventDefault();
 
-  let name = document.getElementById("names").value;
-  let email = document.getElementById("email").value;
-  let setPassword = document.getElementById("setPassword").value;
-  let confirmPassword = document.getElementById("confirmPassword").value;
+//   let name = document.getElementById("names").value;
+//   let email = document.getElementById("email").value;
+//   let setPassword = document.getElementById("setPassword").value;
+//   let confirmPassword = document.getElementById("confirmPassword").value;
 
-  localStorage.getItem("names");
-  localStorage.setItem("names", name);
-  console.log("Name: ", name);
+//   localStorage.getItem("names");
+//   localStorage.setItem("names", name);
+//   console.log("Name: ", name);
 
-  localStorage.getItem("email");
-  localStorage.setItem("email", email);
-  console.log("Email: ", email);
+//   localStorage.getItem("email");
+//   localStorage.setItem("email", email);
+//   console.log("Email: ", email);
 
-  localStorage.getItem("setPassword");
-  localStorage.setItem("setPassword", setPassword);
+//   localStorage.getItem("setPassword");
+//   localStorage.setItem("setPassword", setPassword);
 
-  localStorage.getItem("confirmPassword");
-  localStorage.setItem("confirmPassword", confirmPassword);
-}
+//   localStorage.getItem("confirmPassword");
+//   localStorage.setItem("confirmPassword", confirmPassword);
+// }
